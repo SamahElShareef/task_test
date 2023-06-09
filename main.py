528ce3b747f6504630eb1d4442246842af4b9a0e
SamahElShareef <2004samah2004@gmail.com>
2023-06-09 22:43:35 +0300

    best

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+from studentt_module import Studentt
+student1 = Studentt('ali', 'gaza', 19)
+student2 = Studentt('ali', 'gaza', 19)
+student3 = Studentt('ali', 'gaza', 19)
+from subjectt_module import subjectt
+sub1 = subjectt('arabic', 90)
+sub1 = subjectt('IT', 80)
+sub1 = subjectt('math', 88)
 
-import subjectt_module, studentt_module
-subject1 = subjectt_module
-subject = subject1('english', 89)
-subject = subject1('IT', 80)
-subject = subject1('arabic', 88)
 
-std1 = studentt_module
-st1= std1('ahlam', 'gaza', 23)
-st1= std1('ali', 'gaza', 20)
-st1= std1('ahmad', 'gaza', 18)
